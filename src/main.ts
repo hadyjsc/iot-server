ecc2d2c158296caf78fccb7b87b5a2f06e30342b
Hady Eka Saputra <hadyekasaputra@gmail.com>
2023-07-06 10:03:31 +0700

    securtity handling

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,16 @@ import { ValidationPipe } from '@nestjs/common';
 import { ApplicationContext } from './app.context';
 import { ConfigService } from './commons/configs/config.service';
 import * as cookieParser from 'cookie-parser';
+import helmet from 'helmet'
+import * as csurf from 'csurf';
 
 async function bootstrap() {
   const app = await ApplicationContext();
   app.useGlobalPipes(new ValidationPipe())
   app.use(cookieParser())
-
+  app.enableCors()
+  app.use(helmet())
+  app.use(csurf())
   await app.listen(app.get(ConfigService).get("APP_PORT")).then((v) => {
     console.log(`Server ${app.get(ConfigService).get("APP_NAME")} is running on port ${app.get(ConfigService).get("APP_PORT")}`);
   });
